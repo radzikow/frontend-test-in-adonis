901d58bcf5e46744ad08675141eda5beb8f04ae0
Radoslaw Bura <radoslaw.bura@gmail.com>
2021-06-28 16:35:49 +0200

    refactor clone bacon img

--- resources/js/bacon.js
+++ resources/js/bacon.js
@@ -1,29 +1,15 @@
-const baconImage = document.querySelector('#baconPage img')
-const baconButtonWrapper = document.querySelector('#baconPage section:nth-child(1)')
-const baconImagesWrapper = document.querySelector('#baconPage section:nth-child(2)')
-const baconButton = document.querySelector('#baconPage section:nth-child(1) button')
+const cloneBaconBtn = document.querySelector('.bacon-page section:nth-child(1) button')
 
-// Add classes to the elements on the bacon page
-if (baconImage) {
-  baconImage.classList.add('bacon-image')
-}
-if (baconImagesWrapper) {
-  baconImagesWrapper.classList.add('bacon-images-wrapper')
-}
-if (baconButton) {
-  baconButton.classList.add('bacon-button')
+if (cloneBaconBtn) {
+  cloneBaconBtn.addEventListener('click', cloneBaconImage)
 }
 
-// Handle clone button click
-if (baconButton) {
-  baconButton.addEventListener('click', cloneImage)
-}
+const baconImage = document.querySelector('img[src="/images/bacon.jpg"]')
 
-function cloneImage () {
-  let clone = baconImage.cloneNode(true)
-  baconImage.after(clone)
+function cloneBaconImage () {
+  let clonedImage = baconImage.cloneNode(true)
+  baconImage.after(clonedImage)
   jumpToPageBottom()
-  console.log('Yeah, I love bacon!')
 }
 
 function jumpToPageBottom () {
